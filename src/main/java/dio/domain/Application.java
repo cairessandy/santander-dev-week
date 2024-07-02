41785e2277532531a876034b0d9f7c3fde603119
Sandy Caires <cairessandys@gmail.com>
2024-07-02 15:32:24 -0300

    criação de pacotes

--- src/main/java/dio/domain/Application.java
+++ src/main/java/dio/domain/Application.java
@@ -1,13 +1,13 @@
-package dio;
+package dio.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevWeekApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeekApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
